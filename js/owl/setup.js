623a6d839aec1ff9fe962b8f4a0cf31f9db9a508
taissantos22 <taismariaramos@hotmail.com>
2022-08-10 19:53:21 -0300

    -
    -

---
+++ js/owl/setup.js
@@ -0,0 +1,33 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+    merge:true,
+    loop:true,
+    margin:10,
+    video:true,
+    lazyLoad:true,
+    center:true,
+    responsive:{
+        480:{
+            items:2
+        },
+        600:{
+            items:4
+        }
+    }
+})
